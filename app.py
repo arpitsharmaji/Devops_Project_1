27c6162bcefb2a15790ec8eec5655aecb69d4311
arpitsharmaji <sharmaarpit9690@gmail.com>
2025-10-08 11:05:17 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route("/info")
 def lwinfo():
-    return "I am arpit from Saharanpur"
+    return "I am Arpit from Saharanpur"
 
 @app.route("/phone")
 def lwphone():
     return "9084511318"
 
-app.run(host="0.0.0.0")
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
 
