46e842210389161b7a4c74c125283b7c2dfef7f6
arpitsharmaji <sharmaarpit9690@gmail.com>
2025-10-08 10:40:21 +0530

    updated

--- first.py
+++
@@ -1,9 +0,0 @@
-def lwsum(x,y):
-    z = x + y
-    return z
-
-def lwmult(a,b):
-    c = a * b
-    return c
-
-
--- test_first.py
+++
@@ -1,8 +0,0 @@
-from first import lwsum
-
-def test_lwsum():
-    i = 4
-    j = 5
-    assert lwsum(i,j) == 9
-    
-
